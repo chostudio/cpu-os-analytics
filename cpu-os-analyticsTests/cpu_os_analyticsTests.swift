9cb1a92fb9e605a8d5a80ca007a628da98956789
Chris H <officialchrisho@gmail.com>
2025-12-25 18:13:15 -1000

    Initial Commit

---
+++ cpu-os-analyticsTests/cpu_os_analyticsTests.swift
@@ -0,0 +1,17 @@
+//
+//  cpu_os_analyticsTests.swift
+//  cpu-os-analyticsTests
+//
+//  Created by Chris Ho on 12/25/25.
+//
+
+import Testing
+@testable import cpu_os_analytics
+
+struct cpu_os_analyticsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
